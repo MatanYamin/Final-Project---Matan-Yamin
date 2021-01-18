f29aba3f085b8fbc32e2162fa4256393172c5f17
MatanYamin <yamin2211@gmail.com>
2021-01-18 15:51:28 +0200

    check modal

--- application/views/appointments/book.php
+++ application/views/appointments/book.php
@@ -101,6 +101,34 @@
                 </div>
 
                 <!-- ENDD CHECKKKK -->
+                <script>
+                // Get the modal
+                var modal = document.getElementById("myModal");
+
+                // Get the button that opens the modal
+                var btn = document.getElementById("myBtn");
+
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+
+                // When the user clicks on the button, open the modal
+                btn.onclick = function() {
+                modal.style.display = "block";
+                }
+
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function() {
+                modal.style.display = "none";
+                }
+
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+                }
+
+                </script>
                 <!-- video section-->
                 <p id="text_first_page" style="text-align: center;
         margin-top: 15%; font-size: 25px; color: white;">‏
