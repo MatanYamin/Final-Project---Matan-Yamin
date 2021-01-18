6917451b808784f6f7ec6d0fa37dbc5d44ffd1cd
MatanYamin <yamin2211@gmail.com>
2021-01-18 15:58:14 +0200

    modal

--- application/views/appointments/book.php
+++ application/views/appointments/book.php
@@ -87,14 +87,14 @@
                 <!-- CHECK!!!!!!! -->
 
                 <!-- try new moal -->
-                <button id="myBtn">Open Modal</button>
+                <button id="myBtn_skycleaner">SOS button</button>
 
                 <!-- The Modal -->
-                <div id="myModal" class="modal">
+                <div id="myModal_skycleaner" class="modal_sky">
 
                 <!-- Modal content -->
-                <div class="modal-content">
-                    <span class="close">&times;</span>
+                <div class="modal-content_sky">
+                    <span class="close_modal_sky">&times;</span>
                     <p>Some text in the Modal..</p>
                 </div>
 
@@ -103,28 +103,28 @@
                 <!-- ENDD CHECKKKK -->
                 <script>
                 // Get the modal
-                var modal = document.getElementById("myModal");
+                var modal_sky = document.getElementById("myModal_skycleaner");
 
                 // Get the button that opens the modal
-                var btn = document.getElementById("myBtn");
+                var btn = document.getElementById("myBtn_skycleaner");
 
                 // Get the <span> element that closes the modal
-                var span = document.getElementsByClassName("close")[0];
+                var span = document.getElementsByClassName("close_modal_sky")[0];
 
                 // When the user clicks on the button, open the modal
                 btn.onclick = function() {
-                modal.style.display = "block";
+                modal_sky.style.display = "block";
                 }
 
                 // When the user clicks on <span> (x), close the modal
                 span.onclick = function() {
-                modal.style.display = "none";
+                modal_sky.style.display = "none";
                 }
 
                 // When the user clicks anywhere outside of the modal, close it
                 window.onclick = function(event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
+                if (event.target == modal_sky) {
+                    modal_sky.style.display = "none";
                 }
                 }
 
